f629d0c08bdf09cc4ab04486ec539c8fc8851229
koisland <koshima789@gmail.com>
2024-05-03 00:14:06 -0400

    Finished cen grouping script. Workflow functional until stringdecomposer.

--- workflow/scripts/group_seq.py
+++ workflow/scripts/group_seq.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import polars as pl
 
@@ -5,15 +6,10 @@ RGX_HAP1 = r"(h1|haplotype1|pat)"
 RGX_HAP2 = r"(h2|haplotype2|mat)"
 
 
-def main():
-    ap = argparse.ArgumentParser(description="")
-    ap.add_argument("-i", "--infile", help="Input centromere contigs by chromosome")
-
-    args = ap.parse_args()
-
-    df = pl.concat(
+def read_cens_info(infile: str) -> pl.DataFrame:
+    df_cens_info = pl.concat(
         pl.scan_csv(
-            r["path"],
+            r["path"] + ".fai",
             separator="\t",
             new_columns=["ctg", "length", "offset", "linebases", "linewidth"],
         )
@@ -21,22 +17,22 @@ def main():
             chr=pl.lit(r["chromosome"]),
             ort=pl.lit(r["orientation"]),
             grp=pl.lit(r["group"]),
+            path=pl.lit(r["path"]),
             ctg_coord=pl.col("ctg").str.splitn(by=":", n=2),
         )
         .unnest("ctg_coord")
-        .rename({"field_0": "ctg_name", "field_1": "ctg_coord"})
-        .drop("ctg", "ctg_coord")
-        .collect()
+        .rename({"field_0": "ctg_name"})
         # Open fasta index
-        for r in pl.read_csv(args.infile, separator="\t", has_header=True)
-        .with_columns(pl.col("path") + ".fai")
+        for r in pl.read_csv(infile, separator="\t", has_header=True)
         .iter_rows(named=True)
-    )
-    df = (
-        df.with_columns(
+    ).collect()
+    return (
+        df_cens_info.with_columns(
             # Expects sm_chr_hap-ctg
             # hap-ctg must not contains underscores.
-            sm=pl.Series([ctg_name.rsplit("_", 2)[0] for ctg_name in df["ctg_name"]]),
+            sm=pl.Series(
+                [ctg_name.rsplit("_", 2)[0] for ctg_name in df_cens_info["ctg_name"]]
+            ),
             hap1=pl.col("ctg_name").str.extract(RGX_HAP1),
             hap2=pl.col("ctg_name").str.extract(RGX_HAP2),
         )
@@ -47,19 +43,86 @@ def main():
             .then(pl.lit("h1"))
             .otherwise(pl.lit("h2"))
         )
-        .select("ctg_name", "sm", "hap", "grp", "ort", "chr")
+        .select("ctg_name", "ctg", "path", "sm", "hap", "grp", "ort", "chr")
     )
+
+
+def read_misassembled_cens(infile: str) -> pl.Series:
+    return (
+        pl.concat(
+            pl.scan_csv(
+                r["path"],
+                separator="\t",
+                new_columns=["ctg", "start", "stop", "misassembly"],
+            )
+            .with_columns(
+                ctg_coord=pl.col("ctg").str.splitn(by=":", n=2),
+            )
+            .unnest("ctg_coord")
+            .rename({"field_0": "ctg_name", "field_1": "ctg_coord"})
+            .drop("ctg", "ctg_coord")
+            for r in pl.read_csv(infile, separator="\t", has_header=True).iter_rows(
+                named=True
+            )
+        )
+        .collect()
+        .get_column("ctg_name")
+        .unique()
+    )
+
+def read_assembly_contig_lengths(infile: str) -> pl.DataFrame:
+    return (
+        pl.concat(
+            pl.scan_csv(
+                r["path"],
+                separator="\t",
+                new_columns=["ctg_name", "ctg_length", "offset", "linebases", "linewidth"],
+            )
+            for r in pl.read_csv(infile, separator="\t", has_header=True).iter_rows(
+                named=True
+            )
+        )
+        .select("ctg_name", "ctg_length")
+        .collect()
+    )
+
+def main():
+    ap = argparse.ArgumentParser(description="")
+    ap.add_argument("-i", "--infile", required=True, help="Centromere contigs fastas by chromosome, orientation, and group.")
+    ap.add_argument("-m", "--misassemblies", required=True, help="Misassemblies bed files by chromosome and group.")
+    ap.add_argument("-a", "--assembly_faidx", required=True, help="Assembly faidxs by group.")
+    ap.add_argument("-o", "--output_dir", default="output", help="Output dir with contigs")
+
+    args = ap.parse_args()
+
+    srs_misassembled_cens = read_misassembled_cens(args.misassemblies)
+    df_ctg_lengths = read_assembly_contig_lengths(args.assembly_faidx)
+    df_cens_info = read_cens_info(args.infile).join(df_ctg_lengths, on="ctg_name", how="left")
+
+    os.makedirs(args.output_dir)
+
     # Group by sample, haplotype and chromosome.
-    # Filter groups with only one contig.
-    # Filter groups with only one grouping.
-    for grp_name, df_grp in df.group_by(["sm", "hap", "chr"]):
+    for grp_name, df_grp in df_cens_info.group_by(["sm", "hap", "chr"]):
+        # Filter groups with only one contig.
         if df_grp.shape[0] < 2:
             continue
+
+        # Filter groups with only one grouping.
         if len(df_grp["grp"].unique()) == 1:
             continue
-        print(grp_name)
-        print(df_grp)
-    breakpoint()
+
+        # Filter group if no misassemblies.
+        if df_grp.get_column("ctg_name").is_in(srs_misassembled_cens).all():
+            continue
+        
+        df_grp.drop_in_place("ctg_name")
+        df_grp.write_csv(
+            os.path.join(
+                args.output_dir, f"{'_'.join(grp_name)}.tsv"
+            ),
+            separator="\t",
+            include_header=False
+        )
 
 
 if __name__ == "__main__":
